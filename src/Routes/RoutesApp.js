ed8358514c955e2a7362874d31515316cdc7e34d
RobsonFe <robson_20ferreira@hotmail.com>
2023-12-11 21:13:44 -0300

    pagina de favoritos

--- src/Routes/RoutesApp.js
+++ src/Routes/RoutesApp.js
@@ -8,6 +8,7 @@ import Home from "../pages/Home";
 import Filme from "../pages/Filme";
 import Header from "../components/Header";
 import Erro from "../pages/Erro";
+import Favorito from "../pages/Favoritos";
 
 function RoutesApp() {
   return (
@@ -17,6 +18,7 @@ function RoutesApp() {
     <Routes>
         <Route path="/" element={ <Home/> } />
         <Route path="/filme/:id" element={ <Filme/> } />
+        <Route path="/favoritos" element={ <Favorito /> } />
 
         <Route path="*" element={ <Erro/> } />
     </Routes>
---
+++ src/pages/Favoritos/index.js
@@ -0,0 +1,36 @@
+import {useEffect, useState } from 'react'
+import './favoritos.css';
+import { Link } from 'react-router-dom';
+
+function Favorito() {
+
+    const [filmes, setFilmes] = useState([]);
+
+    useEffect(()=>{
+      
+        const minhaLista = localStorage.getItem("@robfilme");
+        setFilmes(JSON.parse(minhaLista) || [])
+    },[])
+
+  return (
+    <div className='meus-filmes'>
+        <h1>Meus Filmes</h1>
+
+        <ul>
+            {filmes.map((item)=>{
+                return(
+                    <li key={item.id}>
+                        <span>{item.title}</span>
+                        <div>
+                        <Link to={`/filme/${item.id}`}>Ver Detalhes</Link>
+                        <button>Excluir</button>
+                        </div>
+                    </li>
+                )
+            })}
+        </ul>
+    </div>
+  )
+}
+
+export default Favorito;
